5b57da5f4a5607c9163df669b6732a30ca588384
Alexey Agafonov <taganroglad1@gmail.com>
2023-04-26 19:42:45 +0300

    just added my personal code

--- main.py
+++ main.py
@@ -1,15 +1,16 @@
 def unique_in_order(number_or_words):
     # Сначала создаем список пустой чтобы можно было все сохранять
-    results = []
+    spisok = []
     # Называем переменную как угодно но она не должна допустить повторения порядка что будет при переборке
-    notnotnot = None
+    nedopusk = None
     # Ищем некоторый item во всех входных на функции
     for item in number_or_words:
-        # если вырезанный нами итем не равен ничему то.... (он потом будет равен текущему значению)
-        if item != notnotnot:
+    # если вырезанный нами итем не равен ничему то.... (он потом будет равен текущему значению)
+        if item != nedopusk:
             # добавляем к тому самому результату значения в список
-            results.append(item)
+            spisok.append(item)
             # теперь то ничто равно текущему значению
-            notnotnot = item
-    return results
-print(unique_in_order('AAAABBBCCDAABBB'))
+            nedopusk = item
+    return  spisok
+
+print(unique_in_order((5, 3, 6, 6, 7, 7)))
